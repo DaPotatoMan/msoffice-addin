cc11f86c83e3c813d42db390f12acfaab6e4febf
dapotatoman <iam.fahadul@gmail.com>
2024-04-04 18:59:05 +0600

    fix: playgound/vite config

--- playground/vite/vite.config.ts
+++ playground/vite/vite.config.ts
@@ -1,15 +1,21 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+
+import MonorepoENV from 'monorepo-env/vite'
 import MSOfficeAddin from '../../src/vite'
 
 export default defineConfig({
   envDir: resolve(__dirname, '..'),
 
   plugins: [
+    MonorepoENV({
+      dts: false,
+    }),
+
     MSOfficeAddin({
       manifests: [
         {
-          src: resolve(__dirname, '../manifest.xml'),
+          src: resolve(__dirname, '../../manifest.xml'),
           route: '/outlook/manifest.xml',
         },
       ],
